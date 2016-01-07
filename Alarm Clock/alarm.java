ab66bcb37619926765411d695ff1d8f56b06ed93
hkumar96 <hemant30nick.com@gmail.com>
2016-01-07 14:18:06 +0530

    fixed the freezing of clock

--- Alarm Clock/alarm.java
+++ Alarm Clock/alarm.java
@@ -4,95 +4,38 @@ import sun.audio.*;
 import java.awt.Color;
 import java.awt.event.*;
 import javax.swing.*;
-class ActionAlarmSet implements ActionListener{
-	JFrame myWin;
-	AlarmClock temp;
-	public ActionAlarmSet(JFrame win, AlarmClock tmp){
-		this.myWin = win;
-		this.temp = tmp;
-	}
-	public void actionPerformed(ActionEvent e){
-		String timeStr;
-		timeStr = JOptionPane.showInputDialog(myWin,"Enter Time(HH:MM:SS)");
-		temp.setAlarm(timeStr);
+import java.awt.*;
 
-	}
-}
 class AlarmClock {
-	GregorianCalendar current, end;
-	long sleepTime;
-	JFrame win;
-	JLabel time;
+	GregorianCalendar currTime,alarmTime;
+	boolean isSet;
+
 	public AlarmClock(){
-		current = new GregorianCalendar();
-		this.initializeFrame();
-	}
-	public void initializeFrame(){
-		final int X_FRAME = 100 ,Y_FRAME = 100 ,LEN_FRAME = 300 ,WID_FRAME = 400;
-		win = new JFrame("Alarm Clock!");
-		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		win.setSize(WID_FRAME,LEN_FRAME);
-		win.setLocation(X_FRAME,Y_FRAME);
-
-		JPanel pane = new JPanel();
-		pane.setLocation(0,100);
-		pane.setSize(WID_FRAME-100,LEN_FRAME-100);
-		pane.setBackground(Color.BLUE);
-
-		time = new JLabel(current.getTime().toString());
-		pane.add(time);
-
-		JButton alarmSet = new JButton("Set Alarm!");
-		alarmSet.addActionListener(new ActionAlarmSet(win,this));
-		pane.add(alarmSet);
-
-		win.add(pane);
-		win.setVisible(true);
-		showElapsedTime();
-
+		currTime = new GregorianCalendar();
+		isSet = false;
 	}
 
-	void showElapsedTime(){
-		showElapsedTime(-1);
+	public String getCurTime(){
+		currTime = new GregorianCalendar();
+		return (currTime.getTime().toString());
 	}
-	void showElapsedTime(long endTime){
-		long prev,next;
-		long startTime = System.currentTimeMillis();
-		prev = System.currentTimeMillis();
-		for(;endTime == -1 || System.currentTimeMillis() - startTime < endTime;){
-			next = System.currentTimeMillis();
-			if(next - prev >= 1000){
-				current = new GregorianCalendar();
-				time.setText(current.getTime().toString());
-				prev = next;
-			}
+
+	public String getAlarmTime(){
+		if (!isSet){
+			JOptionPane.showMessageDialog(null,"ERROR: Alarm not set!");
+			return "--not set--";
 		}
+
+		return (alarmTime.getTime().toString());
 	}
-	public void ringAlarm(){
-		System.out.println("wakeup!");
-		try{
-			String gongFile = "tone.wav";
-	    	InputStream in = new FileInputStream(gongFile);
-	    	try{
-	    		// create an audiostream from the inputstream
-			    AudioStream audioStream = new AudioStream(in);
-			 
-			    // play the audio clip with the audioplayer class
-			    AudioPlayer.player.start(audioStream);
-			    setAlarm(end.get(Calendar.HOUR_OF_DAY),end.get(Calendar.MINUTE),end.get(Calendar.SECOND));
-	    	}catch (IOException ex){
-	    		System.out.println("Unable to open file");
-	    	}
-		}catch (FileNotFoundException ex){
-			System.out.println("File not found");
-		}
-	}
-	public void setAlarm(String timeStr){
+
+	public void setAlarm(String timeStr) {
 		int tempLoc = 0,hh,mm,ss;
 		int count=1;
 		hh = mm = ss = 0;
-		for(int i = 0; i<timeStr.length(); i++){
-			if (timeStr.charAt(i) == ':'){
+
+		for(int i = 0; i<timeStr.length(); i++) {
+			if (timeStr.charAt(i) == ':') {
 				if (count == 1)
 					hh = Integer.parseInt(timeStr.substring(tempLoc,i));
 				else if (count == 2)
@@ -101,62 +44,128 @@ class AlarmClock {
 				tempLoc = i + 1;
 			}
 		}
+
 		if (count == 3) {
 					ss = Integer.parseInt(timeStr.substring(tempLoc,timeStr.length()));
 		}
-		setAlarm(hh,mm,ss);
+
+		this.setAlarm(hh,mm,ss);
 	}
-	public void setAlarm(int hh,int mm,int ss){		
-		end = new GregorianCalendar();
+	private void  setAlarm(int hh, int mm ,int ss) {
 		GregorianCalendar temp = new GregorianCalendar();
 		temp.set(Calendar.HOUR_OF_DAY,hh);
 		temp.set(Calendar.MINUTE,mm);
 		temp.set(Calendar.SECOND,ss);
-		// System.out.println(end.getTime() + "\n" + temp.getTime());
-		if (end.compareTo(temp)>=0){
-			end.set(Calendar.DAY_OF_MONTH,end.get(Calendar.DAY_OF_MONTH)+1);
+		alarmTime = temp;
+
+		currTime = new GregorianCalendar();
+
+		if (alarmTime.compareTo(currTime) <= 0) {
+			alarmTime.set(Calendar.DAY_OF_MONTH, alarmTime.get(Calendar.DAY_OF_MONTH) + 1);
 		}
-		end.set(Calendar.HOUR_OF_DAY,hh);
-		end.set(Calendar.MINUTE,mm);
-		end.set(Calendar.SECOND,ss);
-		// System.out.printf("\nAlarm set for %s\n",end.getTime());
-		current =  new GregorianCalendar();
-		sleepTime = end.getTimeInMillis() - current.getTimeInMillis();
-		// System.out.printf("\nTime remaining %d seconds!\n",(sleepTime/1000));
-		// try{
-		// 	Thread.sleep(sleepTime);
-		// 	ringAlarm();
-		// }catch (InterruptedException ex){
-		// 	Thread.currentThread().interrupt();
-		// }
-		showElapsedTime(sleepTime);
-		ringAlarm();
+
+		this.setIsSet(true);
+	}
+	public boolean getIsSet(){
+		return isSet;
 	}
 
-	
+	private void setIsSet(boolean value) {
+		isSet = value;
+	}
+
+	public void ringAlarm(){
+
+		JOptionPane.showMessageDialog(null,"Alarm ringing!");
+
+		try{
+			String gongFile = "tone.wav";
+	    	InputStream in = new FileInputStream(gongFile);
+	    	try{
+
+	    		// create an audiostream from the inputstream
+
+			    AudioStream audioStream = new AudioStream(in);
+			 
+			    // play the audio clip with the audioplayer class
+
+			    AudioPlayer.player.start(audioStream);
+
+			    setAlarm(alarmTime.get(Calendar.HOUR_OF_DAY),alarmTime.get(Calendar.MINUTE),alarmTime.get(Calendar.SECOND));
+	    	}catch (IOException ex) {
+	    		JOptionPane.showMessageDialog(null,"Unable to open file!");
+	    	}
+		}catch (FileNotFoundException ex){
+			JOptionPane.showMessageDialog(null,"File not found");
+		}
+	}
 }
-public class alarm{
+
+public class alarm extends JFrame implements ActionListener {
+	
+	private JLabel curTimeLbl;
+	private JLabel alarmTimeLbl;
+	private JButton setAlarmBtn;
+	private AlarmClock clock1;
+
 	public static void main(String[] args) {
-		// Scanner s = new Scanner(System.in);
-		// System.out.println("Enter alarm time in format(HH:MM:SS)");
-		// String timeStr = s.nextLine();
-		// int hh=0,mm=0,ss=0,tmp;
-		// int temp = 0;
-		// int count=1;
-		// for(int i = 0; i<timeStr.length(); i++){
-		// 	if (timeStr.charAt(i) == ':'){
-		// 		if (count == 1)
-		// 			hh = Integer.parseInt(timeStr.substring(temp,i));
-		// 		else if (count == 2)
-		// 			mm = Integer.parseInt(timeStr.substring(temp,i));
-		// 		count++;
-		// 		temp = i + 1;
-		// 	}
-		// }
-		// if (count == 3) {
-		// 			ss = Integer.parseInt(timeStr.substring(temp,timeStr.length()));
-		// 		}
-		AlarmClock clock1 = new AlarmClock();
-		// clock1.setAlarm(hh,mm,ss);
+		alarm alarmWin = new alarm();
+		alarmWin.setVisible(true);
+		alarmWin.startAlarm();
+	}
+
+	public alarm(){
+		this.setTitle("Alarm Clock");
+		this.setSize(400,300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		clock1 = new AlarmClock();
+
+		Container contentPane = this.getContentPane();
+		contentPane.setLayout(null);
+		contentPane.setBackground(Color.white);
+
+		curTimeLbl = new JLabel(clock1.getCurTime());
+		curTimeLbl.setBounds(100,20,200,22);
+		curTimeLbl.setBorder(BorderFactory.createLineBorder(Color.white));
+		contentPane.add(curTimeLbl);
+
+		alarmTimeLbl = new JLabel("--not set--");
+		alarmTimeLbl.setBounds(100,42,200,22);
+		alarmTimeLbl.setBorder(BorderFactory.createLineBorder(Color.white));
+		contentPane.add(alarmTimeLbl);
+
+		setAlarmBtn = new JButton("Set Alarm");
+		setAlarmBtn.setBounds(200,64,100,30);
+		setAlarmBtn.setBorder(BorderFactory.createLineBorder(Color.white));
+		setAlarmBtn.addActionListener(this);
+		contentPane.add(setAlarmBtn);
+
+	}
+
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() instanceof JButton) {
+			String inputTime = JOptionPane.showInputDialog(this,"Enter time in HH:MM:SS");
+			if (inputTime.equals("")) {
+				JOptionPane.showMessageDialog(this,"Time not entered. Alarm not set!");
+			}else {
+				clock1.setAlarm(inputTime);
+			}
+		}
+	}
+
+	public void startAlarm() {
+		for(;;){
+			if (!(curTimeLbl.getText().equals(clock1.getCurTime()))){
+				curTimeLbl.setText(clock1.getCurTime());
+			}
+			if (clock1.getIsSet()) {
+				alarmTimeLbl.setText(clock1.getAlarmTime());
+				if (alarmTimeLbl.getText().equals(curTimeLbl.getText())){
+					clock1.ringAlarm();
+				}
+			}
+		}
 	}
 }
